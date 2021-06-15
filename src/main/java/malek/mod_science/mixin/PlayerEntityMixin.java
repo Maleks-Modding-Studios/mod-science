11ff16f0cad4d052f3cac98b37681cf614f54552
MalekiRe <pocmalek@gmail.com>
2021-06-14 22:08:46 -0700

    Added Madness Cardinal Component, also everything is broken, dunno why.

---
+++ src/main/java/malek/mod_science/mixin/PlayerEntityMixin.java
@@ -0,0 +1,34 @@
+package malek.mod_science.mixin;
+
+import malek.mod_science.components.player.madness.Madness;
+import malek.mod_science.util.general.LoggerInterface;
+import net.minecraft.entity.player.PlayerEntity;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(PlayerEntity.class)
+public class PlayerEntityMixin implements LoggerInterface {
+    //*Heavy New-York Accent*
+    //HEY YOU! YA! IM TALKING TO YOU, DA FUCK YOU DOING IN PLAYER ENTITY MIXIN???
+    //If ya wanna get shit done, do it where it counts, so it won't crash the game
+    //The minute we try and launch a server, aka, unless ur doing client specific stuff
+    //Do it in SERVERPLAYERENTITYMIXIN ya doofus.
+    //Okay bye see ya
+    //Also tell ya ma i love her cooking.
+    //Mwa
+
+    @Inject(method = "tick", at = @At("HEAD"), cancellable = true)
+    public void tickMixin(CallbackInfo ci) {
+        log(Madness.get((PlayerEntity)((Object)this)).getMadness() + "");
+        Madness.get((PlayerEntity)((Object)this)).increaseMadness(1);
+    }
+
+    @Override
+    public Logger getLogger() {
+        return LogManager.getLogger();
+    }
+}
