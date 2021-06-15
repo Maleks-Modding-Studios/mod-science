3e0e6cf9d7ae4d34e631cae8176da481d117ffb3
Platymemo <edwardhazelton+github@gmail.com>
2021-06-14 22:08:09 -0800

    cca fixes

--- src/main/java/malek/mod_science/client/ModScienceClient.java
+++ src/main/java/malek/mod_science/client/ModScienceClient.java
@@ -2,11 +2,13 @@ package malek.mod_science.client;
 
 import malek.mod_science.util.general.LoggerInterface;
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 
-@net.fabricmc.api.Environment(net.fabricmc.api.EnvType.CLIENT)
+@Environment(EnvType.CLIENT)
 public class ModScienceClient implements ClientModInitializer, LoggerInterface {
     @Override
     public void onInitializeClient() {
