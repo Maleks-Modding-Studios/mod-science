6e94146d6b2040c13cc2f231634599c111205b1d
Platymemo <edwardhazelton+github@gmail.com>
2021-06-15 22:34:01 -0800

    ley knots now generate at random positions

--- src/main/java/malek/mod_science/generation/LeyKnotFeature.java
+++ src/main/java/malek/mod_science/generation/LeyKnotFeature.java
@@ -4,6 +4,7 @@ import com.mojang.serialization.Codec;
 import malek.mod_science.components.world.ley_knots.LeyKnot;
 import malek.mod_science.components.world.ley_knots.LeyKnotMap;
 import malek.mod_science.util.general.LoggerInterface;
+import net.minecraft.block.Block;
 import net.minecraft.client.world.ClientWorld;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.math.BlockPos;
@@ -19,8 +20,9 @@ import org.apache.logging.log4j.Logger;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Random;
 
-public class LeyKnotFeature extends Feature implements LoggerInterface {
+public class LeyKnotFeature extends Feature<DefaultFeatureConfig> implements LoggerInterface {
     public static final float CHANCE_FOR_LEY_KNOT_TO_GENERATE = 0.1F;
     public static final double MIN_DISTANCE_TO_SPAWN = 100;
     public static final int NUMBER_OF_LEY_KNOTS_NEARBY_CHECKED = 400;
@@ -30,7 +32,7 @@ public class LeyKnotFeature extends Feature implements LoggerInterface {
     }
 
     //This little thing right here makes sure a leyKnot isn't placed to close to the last placed leyKnot.
-    //It also functions independtly in each dimension.
+    //It also functions independently in each dimension.
     public static Map<World, ArrayList<BlockPos>> lastPos = new HashMap<>();
 
     private static void addToLastPos(World world, BlockPos pos) {
@@ -41,53 +43,56 @@ public class LeyKnotFeature extends Feature implements LoggerInterface {
     }
 
     @Override
-    public boolean generate(FeatureContext context) {
+    public boolean generate(FeatureContext<DefaultFeatureConfig> context) {
         World world = context.getWorld().toServerWorld();
+        Random random = context.getRandom();
+        BlockPos targetPos = context.getOrigin().add(random.nextInt(16), 0, random.nextInt(16));
         if (context.getRandom().nextFloat() <= CHANCE_FOR_LEY_KNOT_TO_GENERATE) {
-            if (lastPos.get(world) == null) {
-                lastPos.put(world, new ArrayList<>());
-            }
-            if (lastPos.get(world).size() > 0) {
-                if (context.getOrigin().getSquaredDistance(lastPos.get(world).get(lastPos.get(world).size() - 1)) < MIN_DISTANCE_TO_SPAWN) {
+            lastPos.computeIfAbsent(world, k -> new ArrayList<>());
+            ArrayList<BlockPos> leyKnotList = lastPos.get(world);
+            if (leyKnotList.size() > 0) {
+                if (targetPos.getSquaredDistance(leyKnotList.get(leyKnotList.size() - 1)) < MIN_DISTANCE_TO_SPAWN) {
                     return false;
                 }
             }
 
-            addToLastPos(world, context.getOrigin().mutableCopy());
+            if (leyKnotList.size() == 3) {
+                leyKnotList.clear();
+            }
+            leyKnotList.add(targetPos.toImmutable());
             if (context.getWorld().isClient()) {
                 return true;
             }
-            generateLeyKnot(context);
+            generateLeyKnot(targetPos, context);
             return true;
         }
         return false;
     }
 
-    private void generateLeyKnot(FeatureContext<RandomFeatureConfig> context) {
+    private void generateLeyKnot(BlockPos targetPos, FeatureContext<DefaultFeatureConfig> context) {
         ServerWorld world = context.getWorld().toServerWorld();
+        Map<BlockPos, LeyKnot> leyKnotMap = LeyKnotMap.get(world);
+        ArrayList<BlockPos> lastPosList = lastPos.get(world);
         LeyKnot leyKnot = new LeyKnot();
-        leyKnot.setFields(context.getOrigin().getX(), context.getOrigin().getZ(), 100, 1, 1, false);
-        LeyKnotMap.get(world).put(new BlockPos(context.getOrigin().getX(), 0, context.getOrigin().getZ()), leyKnot);
-        if (lastPos.get(world).size() == 3) {
-            LeyKnotMap.get(world).get(lastPos.get(world).get(0)).setLinkedKnot1(lastPos.get(world).get(1));
-            LeyKnotMap.get(world).get(lastPos.get(world).get(0)).setLinkedKnot2(lastPos.get(world).get(2));
+        leyKnot.setFields(targetPos.getX(), targetPos.getZ(), 100, 1, 1, false);
+        leyKnotMap.put(new BlockPos(targetPos.getX(), 0, targetPos.getZ()), leyKnot);
+        if (lastPosList.size() == 3) {
+            leyKnotMap.get(lastPosList.get(0)).setLinkedKnot1(lastPosList.get(1));
+            leyKnotMap.get(lastPosList.get(0)).setLinkedKnot2(lastPosList.get(2));
 
-            LeyKnotMap.get(world).get(lastPos.get(world).get(1)).setLinkedKnot1(lastPos.get(world).get(0));
-            LeyKnotMap.get(world).get(lastPos.get(world).get(1)).setLinkedKnot2(lastPos.get(world).get(2));
+            leyKnotMap.get(lastPosList.get(1)).setLinkedKnot1(lastPosList.get(0));
+            leyKnotMap.get(lastPosList.get(1)).setLinkedKnot2(lastPosList.get(2));
 
-            LeyKnotMap.get(world).get(lastPos.get(world).get(2)).setLinkedKnot1(lastPos.get(world).get(0));
-            LeyKnotMap.get(world).get(lastPos.get(world).get(2)).setLinkedKnot2(lastPos.get(world).get(1));
+            leyKnotMap.get(lastPosList.get(2)).setLinkedKnot1(lastPosList.get(0));
+            leyKnotMap.get(lastPosList.get(2)).setLinkedKnot2(lastPosList.get(1));
 
-            log("Ley Knots are linked in these pos : " + lastPos.get(world).get(0) + lastPos.get(world).get(1) + lastPos.get(world).get(2));
-
+            log("Ley Knots are linked in these pos : " + lastPosList.get(0) + lastPosList.get(1) + lastPosList.get(2));
         }
-        log("Created Ley Knot At : x, " + context.getOrigin().getX() + ", z, " + context.getOrigin().getZ());
+        log("Created Ley Knot At : x, " + targetPos.getX() + ", z, " + targetPos.getZ());
     }
 
     @Override
     public Logger getLogger() {
         return LogManager.getLogger();
     }
-
-
 }
