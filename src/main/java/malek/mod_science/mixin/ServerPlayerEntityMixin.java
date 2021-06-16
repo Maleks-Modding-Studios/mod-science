81d9807954b37cdb6aeb91940231956497599f6c
Gamma_02 <gammagames06@gmail.com>
2021-06-16 18:41:40 -0500

    did some thingies

--- src/main/java/malek/mod_science/mixin/ServerPlayerEntityMixin.java
+++ src/main/java/malek/mod_science/mixin/ServerPlayerEntityMixin.java
@@ -4,8 +4,13 @@ import com.mojang.authlib.GameProfile;
 import malek.mod_science.components.player.madness.Madness;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.network.MessageType;
 import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.text.LiteralText;
+import net.minecraft.text.Text;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Box;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -13,14 +18,32 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Mixin(ServerPlayerEntity.class)
 public abstract class ServerPlayerEntityMixin extends PlayerEntity {
     @Shadow
-    private Entity cameraEntity;
+    public abstract ServerWorld getServerWorld();
+
+    @Shadow
+    public abstract void sendMessage(Text message, boolean actionBar);
+
+
+
+    static List<Text> whisperText = new ArrayList<>();
 
     public ServerPlayerEntityMixin(World world, BlockPos pos, float yaw, GameProfile profile) {
         super(world, pos, yaw, profile);
+        whisperText.add(new LiteralText(this.getDisplayName().asString() + " seems lost in thought"));
+        whisperText.add(new LiteralText(this.getDisplayName().asString() + " cackled quietly"));
+        whisperText.add(new LiteralText(this.getDisplayName().asString() + " is fiddling with their " + this.getMainHandStack().getName()));
+        whisperText.add(new LiteralText(this.getDisplayName().asString() + "is muttering something under their breath"));
     }
+
+
+
+
 
     @Inject(method = "tick", at = @At("HEAD"))
     public void tickMixin(CallbackInfo ci) {
@@ -32,6 +55,22 @@ public abstract class ServerPlayerEntityMixin extends PlayerEntity {
                     1.0F
             );
         }
+        int playerGetter;
+        int stringSelector;
+        if (Madness.get((ServerPlayerEntity) (Object) this).isMedium()){
+            Box ServerPlayerInRadius = new Box(this.getX()-5,this.getY()-5, this.getZ()-5, this.getX()+5, this.getY()+5, this.getZ()+5);
+            List<Entity> playersToChat = this.getServerWorld().getOtherEntities(null, ServerPlayerInRadius, (entity) -> entity instanceof PlayerEntity);
+
+            for (Entity entity : playersToChat) {
+                playerGetter = ((int) (Math.random() * 0 - 100));
+                stringSelector = ((int) (Math.random() * 0 - 4));
+                if (playerGetter == 5) {
+                    this.getServer().getPlayerManager().broadcastChatMessage(whisperText.get(stringSelector), MessageType.CHAT, entity.getUuid());
+
+                }
+            }
+        }
+
 
     }
 
---
+++ src/main/java/malek/mod_science/mixin/TameableEntityMixin.java
@@ -0,0 +1,17 @@
+package malek.mod_science.mixin;
+
+
+import net.minecraft.entity.passive.TameableEntity;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(TameableEntity.class)
+public class TameableEntityMixin {
+
+    @Inject(method = "canBeLeashedBy", at = @At("HEAD"), cancellable = true)
+    public void canBeLeashedByMixin(CallbackInfoReturnable cir){
+        cir.setReturnValue(true);
+    }
+}
