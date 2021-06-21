b8be774d1f558a8df2718279f4fa8083c39bfe88
MalekiRe <pocmalek@gmail.com>
2021-06-20 19:43:30 -0700

    Added textures and made ore gen sorta work

--- src/main/java/malek/mod_science/items/ModItems.java
+++ src/main/java/malek/mod_science/items/ModItems.java
@@ -15,12 +15,16 @@ import static malek.mod_science.ModScience.MOD_ID;
 public final class ModItems {
 
     public static final ItemGroup MOD_SCIENCE = FabricItemGroupBuilder.create(new Identifier("modscience", "mod_science")).icon(() -> new ItemStack(Items.GLOW_ITEM_FRAME)).build();
+    public static final FabricItemSettings DEFAULT = new FabricItemSettings().group(MOD_SCIENCE);
 
     @RegistryEntry("golem_repair_kit")
     public static final Item GOLEM_REPAIR_KIT = new GolemRepairKit(new FabricItemSettings().group(MOD_SCIENCE));
 
     @RegistryEntry("test_scythe")
     public static final Item TEST_SCYTHE = new ScytheItem(ToolMaterials.IRON, 1, -2.8F, new FabricItemSettings().group(MOD_SCIENCE));
+
+    @RegistryEntry("void_dust")
+    public static final Item VOID_DUST = new Item(DEFAULT);
 
 
     public static void init() {
