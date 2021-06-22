c0003b35aa5539695887fe17db3d0ac1f1d29b1b
Safrodev <70856001+Safrodev@users.noreply.github.com>
2021-06-21 20:23:40 -0400

    Added Shear Force (no functionality + no durability just works as shears)

---
+++ src/main/java/malek/mod_science/items/ShearForce.java
@@ -0,0 +1,11 @@
+package malek.mod_science.items;
+
+import net.minecraft.item.Item;
+import net.minecraft.item.ShearsItem;
+
+public class ShearForce extends ShearsItem {
+    public ShearForce(Item.Settings settings) {
+        super(settings);
+    }
+
+}
