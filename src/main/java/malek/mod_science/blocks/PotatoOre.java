f88ddf7b0f9b324838fab2ab61021e0408853bc2
Safrodev <70856001+Safrodev@users.noreply.github.com>
2021-06-29 13:13:44 -0400

    Add files via upload

---
+++ src/main/java/malek/mod_science/blocks/PotatoOre.java
@@ -0,0 +1,10 @@
+package malek.mod_science.blocks;
+
+import net.minecraft.block.Block;
+
+public class PotatoOre extends Block {
+
+    public PotatoOre(Settings settings) {
+        super(settings);
+    }
+}
