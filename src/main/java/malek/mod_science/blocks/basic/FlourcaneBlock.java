3fb1f97ad0b02599f2ee703aa85448e3fb03a261
Safrodev <70856001+Safrodev@users.noreply.github.com>
2021-07-26 16:23:51 -0400

    Updated blocks

---
+++ src/main/java/malek/mod_science/blocks/basic/FlourcaneBlock.java
@@ -0,0 +1,100 @@
+package malek.mod_science.blocks.basic;
+
+import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
+import net.minecraft.block.*;
+import net.minecraft.client.render.RenderLayer;
+import net.minecraft.fluid.FluidState;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.state.StateManager;
+import net.minecraft.state.property.IntProperty;
+import net.minecraft.state.property.Properties;
+import net.minecraft.tag.BlockTags;
+import net.minecraft.tag.FluidTags;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
+import net.minecraft.util.shape.VoxelShape;
+import net.minecraft.world.BlockView;
+import net.minecraft.world.WorldAccess;
+import net.minecraft.world.WorldView;
+
+import java.util.Iterator;
+import java.util.Random;
+
+public class FlourcaneBlock extends Block {
+    public static final IntProperty AGE;
+    public static final VoxelShape SHAPE;
+
+    public FlourcaneBlock(AbstractBlock.Settings settings) {
+        super(settings);
+        this.setDefaultState((BlockState)((BlockState)this.stateManager.getDefaultState()).with(AGE, 0));
+    }
+
+    public VoxelShape getOutlineShape(BlockState state, BlockView world, BlockPos pos, ShapeContext context) {
+        return SHAPE;
+    }
+
+    public void scheduledTick(BlockState state, ServerWorld world, BlockPos pos, Random random) {
+        if (!state.canPlaceAt(world, pos)) {
+            world.breakBlock(pos, true);
+        }
+
+    }
+
+    public void randomTick(BlockState state, ServerWorld world, BlockPos pos, Random random) {
+        if (world.isAir(pos.up())) {
+            int i;
+            for(i = 1; world.getBlockState(pos.down(i)).isOf(this); ++i) {
+            }
+
+            if (i < 3) {
+                int j = (Integer)state.get(AGE);
+                if (j == 15) {
+                    world.setBlockState(pos.up(), this.getDefaultState());
+                    world.setBlockState(pos, (BlockState)state.with(AGE, 0), Block.NO_REDRAW);
+                } else {
+                    world.setBlockState(pos, (BlockState)state.with(AGE, j + 1), Block.NO_REDRAW);
+                }
+            }
+        }
+
+    }
+
+    public boolean canPlaceAt(BlockState state, WorldView world, BlockPos pos) {
+        BlockState blockState = world.getBlockState(pos.down());
+        if (blockState.isOf(this)) {
+            return true;
+        } else {
+            if (blockState.isIn(BlockTags.DIRT) || blockState.isOf(Blocks.SAND) || blockState.isOf(Blocks.RED_SAND)) {
+                BlockPos blockPos = pos.down();
+                Iterator var6 = Direction.Type.HORIZONTAL.iterator();
+
+                while(var6.hasNext()) {
+                    Direction direction = (Direction)var6.next();
+                    BlockState blockState2 = world.getBlockState(blockPos.offset(direction));
+                    FluidState fluidState = world.getFluidState(blockPos.offset(direction));
+                    if (fluidState.isIn(FluidTags.WATER) || blockState2.isOf(Blocks.FROSTED_ICE)) {
+                        return true;
+                    }
+                }
+            }
+
+            return false;
+        }
+    }
+
+    public BlockState getStateForNeighborUpdate(BlockState state, Direction direction, BlockState neighborState, WorldAccess world, BlockPos pos, BlockPos neighborPos) {
+        if (!state.canPlaceAt(world, pos)) {
+            world.getBlockTickScheduler().schedule(pos, this, 1);
+        }
+        return super.getStateForNeighborUpdate(state, direction, neighborState, world, pos, neighborPos);
+    }
+
+    public void appendProperties(StateManager.Builder<Block, BlockState> builder) {
+        builder.add(AGE);
+    }
+
+    static {
+        AGE = Properties.AGE_15;
+        SHAPE = Block.createCuboidShape(2.0D, 0.0D, 2.0D, 14.0D, 16.0D, 14.0D);
+    }
+}
