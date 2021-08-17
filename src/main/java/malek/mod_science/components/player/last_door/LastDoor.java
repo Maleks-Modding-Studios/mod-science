056eae2fa5f2c907dcf01e5eb30ce66d596df0bc
gamma_02 <gammagames06@gmail.com>
2021-08-16 18:40:29 -0700

    d]io-0qewtregrjiop ;'gaedsrij'p ;adsgrjip ;adsgrb

--- src/main/java/malek/mod_science/components/player/last_door/LastDoor.java
+++ src/main/java/malek/mod_science/components/player/last_door/LastDoor.java
@@ -1,4 +1,4 @@
-package malek.mod_science.components.player.lastDoor;
+package malek.mod_science.components.player.last_door;
 
 import dev.onyxstudios.cca.api.v3.component.ComponentKey;
 import dev.onyxstudios.cca.api.v3.component.ComponentRegistryV3;
@@ -6,10 +6,8 @@ import dev.onyxstudios.cca.api.v3.component.sync.AutoSyncedComponent;
 import dev.onyxstudios.cca.api.v3.entity.EntityComponentFactoryRegistry;
 import dev.onyxstudios.cca.api.v3.entity.EntityComponentInitializer;
 import dev.onyxstudios.cca.api.v3.entity.RespawnCopyStrategy;
-import malek.mod_science.components.player.madness.Madness;
 import malek.mod_science.util.general.NBTUtil;
 import net.minecraft.entity.Entity;
-import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.nbt.NbtCompound;
 import net.minecraft.network.PacketByteBuf;
 import net.minecraft.server.network.ServerPlayerEntity;
@@ -18,14 +16,16 @@ import net.minecraft.util.Identifier;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.registry.RegistryKey;
 import net.minecraft.world.World;
+import org.apache.http.config.Registry;
+import org.lwjgl.system.CallbackI;
 
 import static malek.mod_science.ModScience.*;
 
 public class LastDoor implements LastDoorInterface, EntityComponentInitializer, AutoSyncedComponent {
-    public static final ComponentKey<LastDoor> LAST_DOOR = ComponentRegistryV3.INSTANCE.getOrCreate(ModScienceId("last_door"), LastDoor.class);
+    public static final ComponentKey<LastDoor> LAST_DOOR = ComponentRegistryV3.INSTANCE.getOrCreate(new Identifier(MOD_ID, "last_door"), LastDoor.class);
 
     private BlockPos LastDoor = new BlockPos(0, 0, 0);
-    private ServerWorld world = getServer().getOverworld();
+    private RegistryKey world = getServer().getOverworld().getRegistryKey();
 
     @Override
     public LastDoor getLastDoor(Entity entity){
@@ -33,26 +33,26 @@ public class LastDoor implements LastDoorInterface, EntityComponentInitializer, 
     }
 
     @Override
-    public void setLastWorld(ServerWorld worldd){
+    public void setLastWorld(RegistryKey worldd){
         this.world = worldd;
     }
 
     @Override
     public void writeSyncPacket(PacketByteBuf buf, ServerPlayerEntity recipient) {
         buf.writeBlockPos(LastDoor);
-        buf.writeIdentifier(world.getRegistryKey().getValue());
+        buf.writeIdentifier(world.getValue());
     }
     @Override
     public void applySyncPacket(PacketByteBuf buf) {
         LastDoor = buf.readBlockPos();
-        world = getServer().getWorld(RegistryKey.of(RegistryKey.ofRegistry(buf.readIdentifier()), buf.readIdentifier()));
+        world = RegistryKey.of(RegistryKey.ofRegistry(buf.readIdentifier()), buf.readIdentifier());
     }
 
     public BlockPos getLastDoorBlockPos(){
         return LastDoor;
     }
 
-    public ServerWorld getLastWorld(){return world;}
+    public RegistryKey getLastWorld(){return world;}
 
     @Override
     public void setLastDoor(BlockPos pos) {
@@ -61,15 +61,23 @@ public class LastDoor implements LastDoorInterface, EntityComponentInitializer, 
 
     @Override
     public void readFromNbt(NbtCompound tag) {
-        Identifier yeetusID = new Identifier(tag.getString("last_world"));
+        Identifier WorldID = new Identifier(tag.getString("last_world_namespace"), tag.getString("last_world_path"));
         this.LastDoor = NBTUtil.writeBlockPosFromList(tag.getIntArray("last_door"));
-        this.world = getServer().getWorld(RegistryKey.of(RegistryKey.ofRegistry(yeetusID), yeetusID));
+        this.world = RegistryKey.of(RegistryKey.ofRegistry(WorldID), WorldID);
+        System.out.println(RegistryKey.ofRegistry(WorldID));
+        System.out.println(world);
+    }
+    @Override
+    public Boolean isVoid(){
+        return LastDoor.equals(new BlockPos(0, 0, 0));
     }
 
     @Override
     public void writeToNbt(NbtCompound tag) {
         tag.putIntArray("last_door", NBTUtil.writeBlockPosToList(LastDoor));
-        tag.putString("last_world", world.getRegistryKey().getValue().toString());
+        tag.putString("last_world_namespace", world.getValue().getNamespace());
+        tag.putString("last_world_path", world.getValue().getPath());
+        System.out.println(world);
     }
     @Override
     public void registerEntityComponentFactories(EntityComponentFactoryRegistry registry) {
--- src/main/java/malek/mod_science/components/player/last_door/LastDoorInterface.java
+++ src/main/java/malek/mod_science/components/player/last_door/LastDoorInterface.java
@@ -1,13 +1,14 @@
-package malek.mod_science.components.player.lastDoor;
+package malek.mod_science.components.player.last_door;
 
 import dev.onyxstudios.cca.api.v3.component.Component;
-import net.minecraft.block.Block;
 import net.minecraft.entity.Entity;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.registry.RegistryKey;
 
 public interface LastDoorInterface extends Component {
     public LastDoor getLastDoor(Entity entity);
     void setLastDoor(BlockPos pos);
-    void setLastWorld(ServerWorld world);
+    void setLastWorld(RegistryKey world);
+    public Boolean isVoid();
 }
